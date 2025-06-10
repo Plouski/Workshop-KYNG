4db3243faddc94b2d027df14ef077ac20e4098cb
Yasmineibr1 <yasmine.brahimi321@gmail.com>
2025-06-11 00:51:40 +0200

    articles details

--- src/routes/articles/[id]/+page.svelte
+++ src/routes/articles/[id]/+page.svelte
@@ -7,16 +7,16 @@
 
   const fakeArticles = [
     {
-      id: '1',
-      title: 'Le guide ultime de Svelte',
-      image: 'https://placekitten.com/800/400',
-      content: 'Svelte est un framework JavaScript moderne qui compile votre code en JS pur, pour des performances optimales.'
+      id: '5',
+      title: 'Iphone 15',
+      image:'https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-16-finish-select-202409-6-1inch-pink_FMT_WHH?wid=1280&hei=492&fmt=p-jpg&qlt=80&.v=UXp1U3VDY3IyR1hNdHZwdFdOLzg1V0tFK1lhSCtYSGRqMUdhR284NTN4OHY2WXBWQWdyLzNrNnpHemdWd1VyeDFRUjVZcnNucDN6ME84Sk5KdjNoWnBRcmhuV0xiN2owaDd6ZUdJRlYrRThQVGFCY1d2Mm1kVDh4VitUQTlVdk9UOTZ6YWdIMldrRGtERWFIcmlaL0hR&traceId=1',
+      content: "L'iPhone 15, sorti le 22 septembre 2023."
     },
     {
-      id: '2',
-      title: 'Comprendre le routing dans SvelteKit',
-      image: 'https://placehold.co/800x400/EEE/31343C?text=SvelteKit',
-      content: 'SvelteKit facilite la création d’apps modernes avec un routing basé sur les fichiers et une gestion des données simple.'
+      id: '6',
+      title: 'Iphone 16 PRO',
+      image:'https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-16-finish-select-202409-6-1inch-pink_FMT_WHH?wid=1280&hei=492&fmt=p-jpg&qlt=80&.v=UXp1U3VDY3IyR1hNdHZwdFdOLzg1V0tFK1lhSCtYSGRqMUdhR284NTN4OHY2WXBWQWdyLzNrNnpHemdWd1VyeDFRUjVZcnNucDN6ME84Sk5KdjNoWnBRcmhuV0xiN2owaDd6ZUdJRlYrRThQVGFCY1d2Mm1kVDh4VitUQTlVdk9UOTZ6YWdIMldrRGtERWFIcmlaL0hR&traceId=1',
+      content: "L'iPhone 16 Pro se distingue par son écran Super Retina XDR de 6,1 pouces."
     }
   ];
 
@@ -27,7 +27,7 @@
 </script>
 
 {#if article}
-  <div class="max-w-3xl mx-auto mt-12 p-6 bg-white rounded-2xl shadow-md dark:bg-zinc-900 dark:text-white">
+  <div class="max-w-3xl mx-auto mt-12 p-6 bg-white rounded-2xl shadow-md dark:bg-zinc-500 dark:text-white">
     <h1 class="text-3xl font-bold mb-6 text-center">{article.title}</h1>
     <img
       src={article.image}
@@ -35,11 +35,19 @@
       class="rounded-xl mb-6 shadow-lg"
     />
     <p class="text-lg leading-relaxed">{article.content}</p>
-    <div class="mt-8 text-center">
-      <a href="/home" class="text-blue-600 hover:underline dark:text-blue-400">
-        ← Retour à la liste des articles
-      </a>
-    </div>
+    
+    <div class="mt-8 flex justify-between items-center">
+  <a href="/home" class="text-white-600 hover:underline dark:text-pink-900">
+    ← Retour à la liste des articles
+  </a>
+  <button 
+    class="bg-zinc-800 text-white text-sm px-4 py-2 rounded hover:bg-pink-800 transition"
+    on:click={() => alert('Achat non implémenté')}
+  >
+    Acheter
+  </button>
+</div>
+
   </div>
 {:else}
   <p class="text-center mt-12 text-gray-500">Chargement ou article introuvable...</p>
